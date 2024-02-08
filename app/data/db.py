41e87775ed2bb68c3f43831b0804ba394ae9d330
jaelsonsantos1 <jaelsons860@gmail.com>
2024-02-08 07:30:58 -0300

    Alterações no arquivo db.py

--- app/data/db.py
+++ app/data/db.py
@@ -7,11 +7,7 @@ load_dotenv()
 class Database:
     def __init__(self):
         self.connection = pg.connect(
-            host=os.getenv('DB_HOST'),
-            database=os.getenv('DB_DATABASE'),
-            user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASSWORD'),
-            port=os.getenv('DB_PORT')
+            os.getenv('DATABASE_URL')
         )
 
         self.cursor = self.connection.cursor()
